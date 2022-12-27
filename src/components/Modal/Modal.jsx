d26f11944540a5589d8273a68e614e3c38665fe0
YuliiaMakhovska <y.makhovska@gmail.com>
2022-12-27 17:11:48 +0200

    add useEffect to Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useEffect } from "react";
 import PropTypes from 'prop-types';
 import { createPortal } from "react-dom";
 import {Overlay, ModalWindow, ModalImg} from './Modal.styled'
@@ -6,36 +6,34 @@ import {Overlay, ModalWindow, ModalImg} from './Modal.styled'
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown)
-    }
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown)
-    }
-    handleKeyDown = e => {
+const Modal = ({activeUrl, url, onClose}) => {
+    useEffect(() => {
+    const handleKeyDown = e => {
         if (e.code === 'Escape') {
-            this.props.onClose();
+            onClose();
         }
-    }
-    handleOverlay = e => {
-        if (e.target === e.currentTarget) {
-            this.props.onClose();
         }
-    }
+        window.addEventListener('keydown', handleKeyDown)
+        return () => window.removeEventListener('keydown', handleKeyDown)
 
-    render() {
-        const { activeUrl, url } = this.props;
-        return createPortal(
-            <Overlay onClick={this.handleOverlay}>
+    }, [onClose])
+    
+    const handleOverlay = e => {
+        if (e.target === e.currentTarget) {
+            onClose();
+        }
+    }
+            return createPortal(
+            <Overlay onClick={handleOverlay}>
                 <ModalWindow>
                     <ModalImg src={activeUrl} alt={url} />
                 </ModalWindow>
             </Overlay>,
             modalRoot
         );
-    }
 }
+export default Modal;
+
 Modal.propTypes = {
     activeUrl: PropTypes.string.isRequired,
     url: PropTypes.string.isRequired,
